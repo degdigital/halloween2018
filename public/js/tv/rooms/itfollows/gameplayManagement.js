dfe6c11ca684f9d49c20851fe0a0ece34f96112a
Aaron Ladage <aladage@gmail.com>
2018-10-30 01:00:48 -0500

    number to win is a variable

--- public/js/tv/rooms/itfollows/gameplayManagement.js
+++ public/js/tv/rooms/itfollows/gameplayManagement.js
@@ -12,10 +12,10 @@ const gameplayManagement = (el, settings = {}) => {
     const instructionsTime = 15;
     let buttonCount = 6;
     let users;
-    let numberToWin = 10;
+    let numberToWin = 15;
     let data = {
         instructions: `You're being followed.`,
-        subInstructions: `<div class="pre-subinstructions"><p>To escape, press the button on your phone to complete the 5 tasks on the TV.</p><p>If the timer expires&hellip;so do you.</p><p>Get ready!</p></div>`,
+        subInstructions: `<div class="pre-subinstructions"><p>To escape, press the button on your phone to complete the ${numberToWin} tasks on the TV.</p><p>If the timer expires&hellip;so do you.</p><p>Get ready!</p></div>`,
         correctCount: 0,
         correctTask: null
     };
